0af7100bb441ded3c16985e37602cc2fcdbae51d
Ash Tewari <ash.tewari.name@gmail.com>
2013-01-08 22:31:15 -0500

    First Commit

---
+++ CsvIndexer.Console/Program.cs
@@ -0,0 +1,105 @@
+﻿
+namespace CsvIndexer
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// The CsvIndexer program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The Utf8 encoding
+        /// </summary>
+        private static readonly UTF8Encoding Encoding = new UTF8Encoding();
+
+
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">The args.</param>
+        public static void Main(string[] args)
+        {
+            var testFile = "Apex_BB_Boys_Mites.csv";
+
+            TestOffsets(testFile);
+            
+            var writer = new Indexer(testFile);
+            writer.Create("TeamName", 1);
+            var idx1 = writer.Seek("BISONS");
+
+            var loader = new Indexer(testFile);
+
+            loader.Load("TeamName");
+            var idx2 = loader.Seek("BISONS");
+
+            using (var reader = new FileStream(testFile, FileMode.Open))
+            {
+                if (reader.CanSeek)
+                {
+                    foreach (var ix in idx2)
+                    {
+                        var buffer = new byte[ix[1]];
+                        var seek = reader.Seek(ix[0], SeekOrigin.Begin);
+                        var read = reader.Read(buffer, 0, ix[1]);
+                        string text = DecodeData(buffer);
+                        Trace.WriteLine(text);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests the offsets.
+        /// </summary>
+        /// <param name="csv">The CSV.</param>
+        private static void TestOffsets(string csv)
+        {
+            var offsets = new List<int>();
+            using (var reader = new StreamReader(csv))
+            {
+                int offset = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    offsets.Add(offset);
+                    offset += line.Length + 2; // The 2 is for NewLine(\r\n)
+                }
+
+                offsets.Add(offset); // pick up the last one
+            }
+
+            using (var reader = new FileStream(csv, FileMode.Open))
+            {
+                if (reader.CanSeek)
+                {
+                    int i = 0;
+
+                    while (i + 1 < offsets.Count)
+                    {
+                        int length = offsets[i + 1] - offsets[i];
+                        var buffer = new byte[length];
+                        var seek = reader.Seek(offsets[i], SeekOrigin.Begin);
+                        var read = reader.Read(buffer, 0, length);
+                        string text = DecodeData(buffer);
+                        Trace.WriteLine(text);
+                        i++;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decodes the data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns>The decoded string.</returns>
+        private static string DecodeData(byte[] data)
+        {                        
+            return Encoding.GetString(data);
+        }
+    }
+}
---
+++ CsvIndexer/Indexer.cs
@@ -0,0 +1,181 @@
+﻿namespace CsvIndexer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Manages indexes for a file
+    /// </summary>
+    public class Indexer
+    {
+        /// <summary>
+        /// The file to be indexed
+        /// </summary>
+        private readonly string fileToIndex;
+
+        /// <summary>
+        /// Delimiter for splitting the csv line
+        /// </summary>
+        private readonly char[] csvDelimiter = new char[] { ',' };
+
+        /// <summary>
+        /// Delimiter for splitting the index line
+        /// </summary>
+        private readonly char[] indexDelimiter = new char[] { '|' };
+
+        /// <summary>
+        /// file extension
+        /// </summary>
+        private readonly string extension = "fidx";
+
+        /// <summary>
+        /// The file index
+        /// </summary>
+        private Dictionary<string, IList<int[]>> index = new Dictionary<string, IList<int[]>>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Indexer"/> class.
+        /// </summary>
+        /// <param name="fileToIndex">Index this file.</param>
+        public Indexer(string fileToIndex) : this(fileToIndex, true)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Indexer"/> class.
+        /// </summary>
+        /// <param name="fileToIndex">Index of the file to.</param>
+        /// <param name="hasHeader">if set to <c>true</c> [has header].</param>
+        public Indexer(string fileToIndex, bool hasHeader)
+        {
+            this.HasHeader = hasHeader;
+            this.fileToIndex = fileToIndex;          
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has header.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance has header; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasHeader { get; set; }
+
+        /// <summary>
+        /// Creates the index.
+        /// </summary>
+        /// <param name="indexName">Name of the index.</param>
+        /// <param name="columnToIndex">Column to index.</param>
+        public void Create(string indexName, int columnToIndex)
+        {
+            string indexFileName = this.GetIndexFileName(indexName);
+            using (var indexFile = System.IO.File.CreateText(indexFileName))
+            {                                
+                using (var reader = new StreamReader(this.fileToIndex))
+                {
+                    int offset = 0;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var columns = line.Split(this.csvDelimiter);
+                        if (columns.Length - 1 >= columnToIndex)
+                        {
+                            var key = columns[columnToIndex];
+                            key = key.Replace('"', ' ').Trim();
+
+                            if (this.index.ContainsKey(key))
+                            {
+                                this.index[key].Add(new int[] { offset, line.Length });
+                            }
+                            else
+                            {
+                                this.index.Add(key, new List<int[]>() { new int[] { offset, line.Length } });                                
+                            }
+                        }
+
+                        offset += line.Length + 2;
+                    }
+                }
+
+                foreach (var key in this.index.Keys)
+                {
+                    indexFile.Write(string.Format("{0}{1}", key, this.indexDelimiter[0]));
+                    foreach (var item in this.index[key])
+                    {
+                        indexFile.Write(string.Format("{0}{1}{2}{3}", item[0], this.csvDelimiter[0], item[1], this.indexDelimiter[0]));
+                    }
+
+                    indexFile.Write(string.Format("\n"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads the specified index name.
+        /// </summary>
+        /// <param name="indexName">Name of the index.</param>
+        public void Load(string indexName)
+        {
+            string indexFileName = this.GetIndexFileName(indexName);
+            using (var indexFile = System.IO.File.OpenText(indexFileName))
+            {
+                this.index.Clear();
+
+                string line;
+                while ((line = indexFile.ReadLine()) != null)
+                {
+                    var ix = line.Split(this.indexDelimiter);
+                    for (int i = 0; i < ix.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            this.index.Add(ix[0], new List<int[]>());
+                        }
+                        else
+                        {
+                            if (string.IsNullOrEmpty(ix[i]))
+                            {
+                                continue;
+                            }
+
+                            var parts = ix[i].Split(this.csvDelimiter);
+                            this.index[ix[0]].Add(new int[] { Convert.ToInt32(parts[0]), Convert.ToInt32(parts[1]) });
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Seeks the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>
+        /// The list of row offsets , line lengths.
+        /// </returns>
+        public IList<int[]> Seek(string key)
+        {
+            IList<int[]> result = new List<int[]>();
+
+            if (this.index.ContainsKey(key))
+            {
+                foreach (var parts in this.index[key])
+                {
+                    result.Add(new int[] { parts[0], parts[1] });   
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the name of the index file.
+        /// </summary>
+        /// <param name="indexName">Name of the index.</param>
+        /// <returns>The name of the index file.</returns>
+        private string GetIndexFileName(string indexName)
+        {
+            return string.Format("{0}.{1}.{2}", System.IO.Path.GetFileNameWithoutExtension(this.fileToIndex), indexName, this.extension);
+        }
+    }
+}
